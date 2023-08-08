9f2ba795028482981562f3916680babe94f4fc82
Sean Egan <segan@cm-logic.com>
2023-08-08 20:12:15 +0100

    added support to enable/disable HPSM

--- pkg/service/scanning_service_test.go
+++ pkg/service/scanning_service_test.go
@@ -251,6 +251,13 @@ func TestScanDirectThreaded(t *testing.T) {
 			file:      "./tests/fingers.wfp",
 			want:      http.StatusOK,
 		},
+		{
+			name:      "Scanning - HPSM success",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "file",
+			file:      "./tests/fingers-hpsm.wfp",
+			want:      http.StatusOK,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -276,6 +283,99 @@ func TestScanDirectThreaded(t *testing.T) {
 			}
 			if _, err = io.Copy(writer, file); err != nil {
 				t.Fatal(err)
+			}
+			_ = mw.Close() // close the writer before making the request
+
+			req := httptest.NewRequest(http.MethodPost, "http://localhost/api/api/scan/direct", postBody)
+			w := httptest.NewRecorder()
+			req.Header.Add("Content-Type", mw.FormDataContentType())
+			apiService.ScanDirect(w, req)
+			resp := w.Result()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("an error was not expected when reading from request: %v", err)
+			}
+			assert.Equal(t, test.want, resp.StatusCode)
+			fmt.Println("Status: ", resp.StatusCode)
+			fmt.Println("Type: ", resp.Header.Get("Content-Type"))
+			fmt.Println("Body: ", string(body))
+		})
+	}
+}
+
+func TestScanDirectSingleHPSM(t *testing.T) {
+	err := zlog.NewSugaredDevLogger()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a sugared logger", err)
+	}
+	defer zlog.SyncZap()
+	myConfig := setupConfig(t)
+	myConfig.App.Trace = true
+	myConfig.Scanning.ScanDebug = true
+	myConfig.Scanning.HPSMEnabled = false
+	apiService := NewAPIService(myConfig)
+
+	tests := []struct {
+		name      string
+		fieldName string
+		file      string
+		binary    string
+		scanType  string
+		assets    string
+		want      int
+	}{
+		{
+			name:      "Scanning - success 1",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "file",
+			file:      "./tests/fingers.wfp",
+			want:      http.StatusOK,
+		},
+		{
+			name:      "Scanning - HPSM fail",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers-hpsm.wfp",
+			want:      http.StatusForbidden,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.want == http.StatusOK { // flip between Trace for some successful calls
+				if myConfig.App.Trace {
+					myConfig.App.Trace = false
+				} else {
+					myConfig.App.Trace = true
+				}
+			} else {
+				myConfig.App.Trace = true
+			}
+			myConfig.Scanning.ScanBinary = test.binary
+			filePath := test.file
+			fieldName := test.fieldName
+			postBody := new(bytes.Buffer)
+			mw := multipart.NewWriter(postBody)
+			file, err := os.Open(filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			writer, err := mw.CreateFormFile(fieldName, filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if _, err = io.Copy(writer, file); err != nil {
+				t.Fatal(err)
+			}
+			if len(test.scanType) > 0 && len(test.assets) > 0 {
+				fmt.Println("Adding asset options: ", test.scanType, test.assets)
+				err = mw.WriteField("type", test.scanType)
+				if err != nil {
+					t.Fatal(err)
+				}
+				err = mw.WriteField("assets", test.assets)
+				if err != nil {
+					t.Fatal(err)
+				}
 			}
 			_ = mw.Close() // close the writer before making the request
 
