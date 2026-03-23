2ab78876f671dcb2e47be43591090df0d165d7a9
eeisegn <eeisegn@gmail.com>
2026-03-23 09:17:46 +0000

    add timeout 504 handling for single file, single worker scan requests. Update unit tests to validate settings

--- pkg/service/scanning_service_test.go
+++ pkg/service/scanning_service_test.go
@@ -112,17 +112,21 @@ func TestScanDirectSingle(t *testing.T) {
 	myConfig := setupConfig(t)
 	myConfig.App.Trace = true
 	myConfig.Scanning.ScanDebug = true
+	myConfig.Scanning.MatchConfigAllowed = false
+	myConfig.Scanning.RankingAllowed = false
 	apiService := NewAPIService(myConfig)
 
 	tests := []struct {
-		name      string
-		fieldName string
-		file      string
-		binary    string
-		telemetry bool
-		scanType  string
-		assets    string
-		want      int
+		name            string
+		fieldName       string
+		file            string
+		binary          string
+		telemetry       bool
+		scanType        string
+		assets          string
+		scanSettingsB64 string
+		settingsAllowed bool
+		want            int
 	}{
 		{
 			name:      "Scanning - wrong name",
@@ -186,6 +190,77 @@ func TestScanDirectSingle(t *testing.T) {
 			assets:    "pkg:github/org/repo",
 			want:      http.StatusOK,
 		},
+		{
+			name:            "Scanning - Settings - invalid base64",
+			binary:          "../../test-support/scanoss.sh",
+			fieldName:       "filename",
+			file:            "./tests/fingers.wfp",
+			settingsAllowed: true,
+			scanSettingsB64: "invalid-base64!!!",
+			want:            http.StatusBadRequest,
+		},
+		{
+			name:      "Scanning - Settings - invalid json",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers.wfp",
+			// Base64 decoded JSON:
+			// {
+			//   "field": "something,
+			//   "array": [
+			// }
+			scanSettingsB64: "ewoiZmllbGQiOiAic29tZXRoaW5nLAogImFycmF5IjogWwp9",
+			settingsAllowed: true,
+			want:            http.StatusBadRequest,
+		},
+		{
+			name:      "Scanning - Settings - not allowed",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers.wfp",
+			// Base64 decoded JSON:
+			// {
+			//   "min_snippet_hits": 5,
+			//   "min_snippet_lines": 10
+			// }
+			scanSettingsB64: "eyJtaW5fc25pcHBldF9oaXRzIjo1LCJtaW5fc25pcHBldF9saW5lcyI6MTB9",
+			settingsAllowed: false,
+			want:            http.StatusBadRequest,
+		},
+		{
+			name:      "Scanning - Settings - success 1",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers.wfp",
+			// Base64 decoded JSON:
+			// {
+			//   "ranking_enabled": true,
+			//   "ranking_threshold": 85,
+			//   "min_snippet_hits": 3,
+			//   "min_snippet_lines": 8,
+			//   "honour_file_exts": false
+			// }
+			scanSettingsB64: "eyJyYW5raW5nX2VuYWJsZWQiOnRydWUsInJhbmtpbmdfdGhyZXNob2xkIjo4NSwibWluX3NuaXBwZXRfaGl0cyI6MywibWluX3NuaXBwZXRfbGluZXMiOjgsImhvbm91cl9maWxlX2V4dHMiOmZhbHNlfQ==",
+			settingsAllowed: true,
+			want:            http.StatusOK,
+		},
+		{
+			name:      "Scanning - Settings - success 2",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers.wfp",
+			// Base64 decoded JSON:
+			// {
+			//   "ranking_enabled": true,
+			//   "ranking_threshold": -1,
+			//   "min_snippet_hits": 3,
+			//   "min_snippet_lines": 8,
+			//   "honour_file_exts": true
+			// }
+			scanSettingsB64: "ewogICJyYW5raW5nX2VuYWJsZWQiOiB0cnVlLAogICJyYW5raW5nX3RocmVzaG9sZCI6IC0xLAogICJtaW5fc25pcHBldF9oaXRzIjogMywKICAibWluX3NuaXBwZXRfbGluZXMiOiA4LAogICJob25vdXJfZmlsZV9leHRzIjogdHJ1ZQp9",
+			settingsAllowed: true,
+			want:            http.StatusOK,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -196,6 +271,8 @@ func TestScanDirectSingle(t *testing.T) {
 					myConfig.App.Trace = true
 				}
 			}
+			myConfig.Scanning.MatchConfigAllowed = test.settingsAllowed
+			myConfig.Scanning.RankingEnabled = test.settingsAllowed
 			myConfig.Scanning.ScanBinary = test.binary
 			myConfig.Telemetry.Enabled = test.telemetry
 			filePath := test.file
@@ -225,9 +302,11 @@ func TestScanDirectSingle(t *testing.T) {
 				}
 			}
 			_ = mw.Close() // close the writer before making the request
-
 			req := httptest.NewRequest(http.MethodPost, "http://localhost/scan/direct", postBody)
 			w := httptest.NewRecorder()
+			if len(test.scanSettingsB64) > 0 {
+				req.Header.Set("Scanoss-Settings", test.scanSettingsB64)
+			}
 			req.Header.Add("Content-Type", mw.FormDataContentType())
 			apiService.ScanDirect(w, req)
 			resp := w.Result()
@@ -430,6 +509,79 @@ func TestScanDirectSingleHPSM(t *testing.T) {
 				if err != nil {
 					t.Fatal(err)
 				}
+			}
+			_ = mw.Close() // close the writer before making the request
+
+			req := httptest.NewRequest(http.MethodPost, "http://localhost/scan/direct", postBody)
+			w := httptest.NewRecorder()
+			req.Header.Add("Content-Type", mw.FormDataContentType())
+			apiService.ScanDirect(w, req)
+			resp := w.Result()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("an error was not expected when reading from request: %v", err)
+			}
+			assert.Equal(t, test.want, resp.StatusCode)
+			fmt.Println("Status: ", resp.StatusCode)
+			fmt.Println("Type: ", resp.Header.Get("Content-Type"))
+			fmt.Println("Body: ", string(body))
+		})
+	}
+}
+
+func TestScanDirectSingleSlow(t *testing.T) {
+	err := zlog.NewSugaredDevLogger()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a sugared logger", err)
+	}
+	defer zlog.SyncZap()
+	myConfig := setupConfig(t)
+	myConfig.App.Trace = true
+	myConfig.Scanning.ScanDebug = true
+	myConfig.Scanning.ScanTimeout = 5
+	apiService := NewAPIService(myConfig)
+
+	tests := []struct {
+		name      string
+		fieldName string
+		file      string
+		binary    string
+		scanType  string
+		assets    string
+		want      int
+	}{
+		{
+			name:      "Scanning - success 1",
+			binary:    "../../test-support/scanoss.sh",
+			fieldName: "file",
+			file:      "./tests/fingers.wfp",
+			want:      http.StatusOK,
+		},
+		{
+			name:      "Scanning - Slow fail",
+			binary:    "../../test-support/scanoss-slow.sh",
+			fieldName: "filename",
+			file:      "./tests/fingers-hpsm.wfp",
+			want:      http.StatusGatewayTimeout,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			myConfig.Scanning.ScanBinary = test.binary
+			filePath := test.file
+			fieldName := test.fieldName
+			postBody := new(bytes.Buffer)
+			mw := multipart.NewWriter(postBody)
+			file, err := os.Open(filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			writer, err := mw.CreateFormFile(fieldName, filePath)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if _, err = io.Copy(writer, file); err != nil {
+				t.Fatal(err)
 			}
 			_ = mw.Close() // close the writer before making the request
 
